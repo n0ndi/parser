fdfc849728bbf8f3abbb5c6a6e44b2a54b756db2
n0ndi <92996169+n0ndi@users.noreply.github.com>
2023-03-18 15:46:57 +0300

    правки 2
    удалил не нужный, отформатировал по pep8

--- render_pages.py
+++ render_pages.py
@@ -1,7 +1,5 @@
 import json
 from more_itertools import chunked
-from pprint import pprint
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -15,21 +13,17 @@ def render_pages():
 
     with open("books_info.json", "r") as file:
         books = json.load(file)
-
-
-
     books = chunked(books, 2)
     pages = list(chunked(books, 10))
-
     for number, page in enumerate(pages):
         rendered_page = template.render(
             books=page,
             pages=len(pages),
             current_page=number,
         )
-
         with open(f'pages/index{number}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     render_pages()
